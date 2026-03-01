3c6c90c5d1be048364a4487066e362fe0d0009d2
MaciejWojs <misiek111403@gmail.com>
2026-03-01 22:29:32 +0100

    feat: add ESLint configuration and scripts for linting
    - Created a new ESLint configuration file (eslint.config.mjs) with recommended settings for ESLint and TypeScript.
    - Updated package.json to include ESLint and TypeScript dependencies.
    - Added lint and lint:fix scripts to package.json for code linting.
    - Refactored src/index.ts for consistent code style and improved readability.

--- src/index.ts
+++ src/index.ts
@@ -1,28 +1,33 @@
-import { Hono } from "hono";
-import { initSocket, getEngine } from "./socket";
+import { Hono } from 'hono';
+import { initSocket, getEngine } from './socket';
 
 const app = new Hono();
 
 initSocket();
 const engine = getEngine();
 
-app.get("/", (c) => c.text("This is the BuddySzponter backend. WebSocket endpoint is at /socket.io/"));
+app.get('/', (c) =>
+  c.text(
+    'This is the Buddy Szponter backend. WebSocket endpoint is at /socket.io/',
+  ),
+);
 
 const { websocket } = engine.handler();
 
 export default {
   port: 3000,
-  idleTimeout: 30, 
+  idleTimeout: 30,
 
-  fetch(req: any, server: any) {
+  //@ts-expect-error Its from Socket.IO Bun Engine github example, but it seems to be missing from types
+  fetch(req, server) {
     const url = new URL(req.url);
 
-    if (url.pathname === "/socket.io/") {
+    if (url.pathname === '/socket.io/') {
       return engine.handleRequest(req, server);
     } else {
       return app.fetch(req, server);
     }
   },
 
-  websocket
-}
+  websocket,
+};
