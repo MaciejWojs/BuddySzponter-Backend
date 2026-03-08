47eafbe308dd9ad5ef020e382c46a8131ff0531a
MaciejWojs <misiek111403@gmail.com>
2026-03-08 13:40:22 +0100

    fix: Implement copilot PR19 suggestions

--- src/modules/auth/api/auth.routes.ts
+++ src/modules/auth/api/auth.routes.ts
@@ -1,17 +1,21 @@
 import { Hono } from 'hono';
 import { loginBodySchema, registerBodySchema } from './auth.schema';
 import { zValidator } from 'src/shared/api/middleware/validator-wrapper';
-import logger from '@logger';
 import {
   InternalServerErrorResponse,
+  loginPayloadSchema,
+  logoutPayloadSchema,
+  mePayloadSchema,
+  refreshPayloadSchema,
   registerPayloadSchema,
 } from './schemas/auth.responses.schema';
 import { StatusCodes } from 'http-status-codes';
 const authRouter = new Hono();
 
 authRouter.post('/register', zValidator('json', registerBodySchema), (c) => {
-  const data = c.req.valid('json');
-  logger.info('Received registration data:', data);
+  // Example validated data access
+  //const data = c.req.valid('json');
+
   // Here you would handle the logic for registering a new user, such as validating input and storing user data.
 
   const payload = {
@@ -31,14 +35,11 @@ authRouter.post('/register', zValidator('json', registerBodySchema), (c) => {
 });
 
 authRouter.post('/login', zValidator('json', loginBodySchema), (c) => {
-  const data = c.req.valid('json');
-  logger.info('Received login data:', data);
-
   const payload = {
     message: 'User logged in successfully',
   };
 
-  const result = registerPayloadSchema.safeParse(payload);
+  const result = loginPayloadSchema.safeParse(payload);
 
   if (!result.success) {
     const internalError: InternalServerErrorResponse = {
@@ -55,7 +56,7 @@ authRouter.post('/refresh', (c) => {
     message: 'Token refreshed successfully',
   };
 
-  const result = registerPayloadSchema.safeParse(payload);
+  const result = refreshPayloadSchema.safeParse(payload);
 
   if (!result.success) {
     const internalError: InternalServerErrorResponse = {
@@ -72,7 +73,7 @@ authRouter.post('/logout', (c) => {
     message: 'User logged out successfully',
   };
 
-  const result = registerPayloadSchema.safeParse(payload);
+  const result = logoutPayloadSchema.safeParse(payload);
 
   if (!result.success) {
     const internalError: InternalServerErrorResponse = {
@@ -89,7 +90,7 @@ authRouter.get('/me', (c) => {
     message: 'Authenticated user information retrieved successfully',
   };
 
-  const result = registerPayloadSchema.safeParse(payload);
+  const result = mePayloadSchema.safeParse(payload);
 
   if (!result.success) {
     const internalError: InternalServerErrorResponse = {
