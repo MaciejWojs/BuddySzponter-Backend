07d22e6eef2766b78870f3612cea98b54cf395ec
ShiroKitsu <hubeerziemba@gmail.com>
2026-03-11 16:18:32 +0100

    fix: remove unnecessary blank lines for improved code clarity in dbDaoFactory

--- src/infrastucture/factories/dbDaoFactory.ts
+++ src/infrastucture/factories/dbDaoFactory.ts
@@ -1,9 +1,7 @@
 import { DrizzleUserDao } from '@modules/users/infrastructure/dao/user.dao';
 
 import { DrizzleDevicesDao } from '@/modules/devices/infrastructure/dao/devices.dao';
-
 import { DrizzleRoleDao } from '@/modules/roles/infrastructure/dao/roles.dao';
-
 import { DrizzleSessionDao } from '@/modules/sessions/infratsucture/dao/session.dao';
 
 export class DbDaoFactory {
