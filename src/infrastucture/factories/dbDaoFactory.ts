27af63aa3bc2342b692b5b6cfdb80b577dacaf9a
ShiroKitsu <hubeerziemba@gmail.com>
2026-03-11 16:14:46 +0100

    feat: add IDevicesDAO and DrizzleDevicesDao for device management

--- src/infrastucture/factories/dbDaoFactory.ts
+++ src/infrastucture/factories/dbDaoFactory.ts
@@ -1,11 +1,15 @@
 import { DrizzleUserDao } from '@modules/users/infrastructure/dao/user.dao';
-
 import { DrizzleRoleDao } from '@/modules/roles/infrastructure/dao/roles.dao';
 import { DrizzleSessionDao } from '@/modules/sessions/infratsucture/dao/session.dao';
+import { DrizzleDevicesDao } from '@/modules/devices/infrastructure/dao/devices.dao';
 
 export class DbDaoFactory {
   userDao() {
     return new DrizzleUserDao();
+  }
+
+  devicesDao() {
+    return new DrizzleDevicesDao();
   }
 
   sessionDao() {
