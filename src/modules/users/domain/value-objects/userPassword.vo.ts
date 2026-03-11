3d238858fc7c2fcadbb96b02835e65fd57954250
MaciejWojs <misiek111403@gmail.com>
2026-03-11 21:20:52 +0100

    feat: enhance password validation with detailed error messages and checks

--- src/modules/users/domain/value-objects/userPassword.vo.ts
+++ src/modules/users/domain/value-objects/userPassword.vo.ts
@@ -3,15 +3,21 @@ import zxcvbn from 'zxcvbn';
 
 export class Password {
   static readonly MIN_SCORE = 3;
+  static readonly MAX_LENGTH = 255;
+  static readonly MIN_LENGTH = 8;
+  static readonly HASH_ALGORITHM = 'argon2id';
 
-  private constructor(private readonly pass: string) {}
+  private constructor(private readonly hashedPass: string) {}
 
   static async create(raw: string): Promise<Password> {
-    const { score } = zxcvbn(raw);
-    if (score < this.MIN_SCORE) throw new Error('Password is too weak');
+    raw = raw.trim();
+    const passwordError = this.checkPasswordRequirements(raw);
+    if (passwordError) {
+      throw passwordError;
+    }
 
     const hashed = await password.hash(raw, {
-      algorithm: 'argon2id',
+      algorithm: this.HASH_ALGORITHM,
     });
 
     return new Password(hashed);
@@ -22,10 +28,50 @@ export class Password {
   }
 
   get value(): string {
-    return this.pass;
+    return this.hashedPass;
   }
 
   async verify(pass: string): Promise<boolean> {
-    return await password.verify(pass, this.pass);
+    return await password.verify(pass, this.hashedPass);
+  }
+
+  private static checkPasswordRequirements(pass: string): Error | false {
+    const { score, feedback } = zxcvbn(pass);
+    if (score < this.MIN_SCORE) {
+      return new Error(
+        `Password is too weak: ${feedback.suggestions.join(' ')}`,
+      );
+    }
+
+    if (pass.length < this.MIN_LENGTH) {
+      return new Error(
+        `Password must be at least ${this.MIN_LENGTH} characters long`,
+      );
+    }
+
+    if (pass.length > this.MAX_LENGTH) {
+      return new Error(
+        `Password must be at most ${this.MAX_LENGTH} characters long`,
+      );
+    }
+
+    if (pass === pass[0]!.repeat(pass.length)) {
+      return new Error('Password cannot consist of the same character');
+    }
+
+    const hasLower = /[a-z]/.test(pass);
+    const hasUpper = /[A-Z]/.test(pass);
+    const hasDigit = /\d/.test(pass);
+    const hasSpecialChar = /[!@#$%^&*(),.?":{}|<>]/.test(pass);
+
+    if (!hasUpper)
+      return new Error('Password must contain at least one uppercase letter');
+    if (!hasLower)
+      return new Error('Password must contain at least one lowercase letter');
+    if (!hasDigit) return new Error('Password must contain at least one digit');
+    if (!hasSpecialChar)
+      return new Error('Password must contain at least one special character');
+
+    return false;
   }
 }
