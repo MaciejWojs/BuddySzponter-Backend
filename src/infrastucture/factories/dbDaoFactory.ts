638c5a44f83bc2cc582ea69d5c411baaa4508f88
ShiroKitsu <hubeerziemba@gmail.com>
2026-03-11 15:43:54 +0100

    feat: implement session DAO with CRUD operations and session retrieval methods

--- src/infrastucture/factories/dbDaoFactory.ts
+++ src/infrastucture/factories/dbDaoFactory.ts
@@ -1,7 +1,12 @@
 import { DrizzleUserDao } from '@modules/users/infrastructure/dao/user.dao';
+import { DrizzleSessionDao } from '@/modules/sessions/infratsucture/dao/session.dao';
 
 export class DbDaoFactory {
   userDao() {
     return new DrizzleUserDao();
+  }
+
+  sessionDao() {
+    return new DrizzleSessionDao();
   }
 }
