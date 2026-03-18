01af57b2d34ea8f4baefeda105ddc1c3e225dca5
MaciejWojs <misiek111403@gmail.com>
2026-03-18 19:58:43 +0100

    feat: update loginUser use case to return User object and handle errors

--- src/modules/auth/application/use-cases/loginUser.ts
+++ src/modules/auth/application/use-cases/loginUser.ts
@@ -1,4 +1,5 @@
 import { IUserRepository } from '@/modules/users/domain/repositories/IUserRepository';
+import { User } from '@/modules/users/domain/entities/User.entity';
 import { Email } from '@/modules/users/domain/value-objects';
 
 import { LoginInput } from '../../api/schemas/auth.requests.schema';
@@ -6,14 +7,21 @@ import { LoginInput } from '../../api/schemas/auth.requests.schema';
 export class LoginUser {
   constructor(private readonly userRepository: IUserRepository) {}
 
-  async execute(input: LoginInput): Promise<void> {
+  async execute(input: LoginInput): Promise<User> {
     const email = new Email(input.email);
 
-    const user = await this.userRepository.findByEmail(email);
+    let user: User;
+    try {
+      user = await this.userRepository.findByEmail(email);
+    } catch {
+      throw new Error('Invalid credentials');
+    }
 
     const isPasswordValid = await user.password.verify(input.password);
     if (!isPasswordValid) {
       throw new Error('Invalid credentials');
     }
+
+    return user;
   }
 }
