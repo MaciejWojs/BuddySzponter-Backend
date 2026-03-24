df54386969baa5607b19e9e0e72fe70a194b1e4e
MaciejWojs <misiek111403@gmail.com>
2026-03-24 01:04:51 +0100

    refactor: reorganize imports in IDeviceRepository, injectIpAddress, and defaultHook

--- src/modules/devices/domain/repositories/IDeviceRepository.ts
+++ src/modules/devices/domain/repositories/IDeviceRepository.ts
@@ -1,13 +1,14 @@
-import { DeviceUUID, UserId } from "@/shared/value-objects";
-import { Device } from "../entities/Device.entity";
-import { DeviceFingerprint } from "../value-objects";
+import { DeviceUUID, UserId } from '@/shared/value-objects';
+
+import { Device } from '../entities/Device.entity';
+import { DeviceFingerprint } from '../value-objects';
 
 export interface IDevicesRepository {
-    findById(id: DeviceUUID): Promise<Device | null>;
-    create(device: Device): Promise<Device>;
-    deleteById(id: DeviceUUID): Promise<boolean>;
-    save(device: Device): Promise<Device>;
+  findById(id: DeviceUUID): Promise<Device | null>;
+  create(device: Device): Promise<Device>;
+  deleteById(id: DeviceUUID): Promise<boolean>;
+  save(device: Device): Promise<Device>;
 
-    findByFingerprint(fingerprint: DeviceFingerprint): Promise<Device[]>;
-    findByUserId(userId: UserId): Promise<Device[]>;
+  findByFingerprint(fingerprint: DeviceFingerprint): Promise<Device[]>;
+  findByUserId(userId: UserId): Promise<Device[]>;
 }
