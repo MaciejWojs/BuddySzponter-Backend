a9121566cd4334447343c00ed127ca9407634f20
MaciejWojs <misiek111403@gmail.com>
2026-03-25 14:28:06 +0100

    Add ESLint plugins for code quality and formatting

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -5,15 +5,19 @@ import { defineConfig, globalIgnores } from 'eslint/config';
 import simpleImportSort from 'eslint-plugin-simple-import-sort';
 import unusedImports from 'eslint-plugin-unused-imports';
 import tseslint from 'typescript-eslint';
+import perfectionist from 'eslint-plugin-perfectionist';
+import prettier from 'eslint-config-prettier';
 
 export default defineConfig(
   globalIgnores(['.dist/**']),
   eslint.configs.recommended,
   tseslint.configs.recommended,
+  prettier,
   {
     plugins: {
       'unused-imports': unusedImports,
       'simple-import-sort': simpleImportSort,
+      perfectionist: perfectionist,
     },
     rules: {
       '@typescript-eslint/no-unused-vars': [
@@ -23,6 +27,20 @@ export default defineConfig(
           varsIgnorePattern: '^_',
           args: 'after-used',
           argsIgnorePattern: '^_',
+        },
+      ],
+      'perfectionist/sort-classes': [
+        'error',
+        {
+          groups: [
+            'index-signature',
+            'static-property',
+            'property',
+            'constructor',
+            'static-method',
+            'method',
+          ],
+          // 'newlines-between': 'always',
         },
       ],
       'simple-import-sort/imports': 'error',
